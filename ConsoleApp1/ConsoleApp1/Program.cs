cf7bae0cc9aaa6b87557a590a730a7dea1b13a07
s28977 <s28977@pjwstk.edu.pl>
2024-03-23 21:25:47 +0100

    Added function calculating max number in int array and tested it

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -13,3 +13,18 @@ static double Average(int[] arr)
     return sum / arr.Length;
 }
 Console.WriteLine(Average([3, 5, 9, -2]));
+
+static int Max(int[] arr)
+{
+    int max = arr[0];
+    for (int i = 1; i < arr.Length; i++)
+    {
+        if (arr[i] > max)
+        {
+            max = arr[i];
+        }
+    }
+
+    return max;
+}
+Console.WriteLine(Max([3, 5, 9, -2]));
