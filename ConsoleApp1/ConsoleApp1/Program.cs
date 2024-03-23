886c1f17ab506ce00e641ee1a4547680d59a4081
s28977 <s28977@pjwstk.edu.pl>
2024-03-23 21:17:51 +0100

    Added function to calculate average and tested it

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,3 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello, World 3!");
+
+static double Average(int[] arr)
+{
+    double sum = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        sum += arr[i];
+    }
+
+    return sum / arr.Length;
+}
+Console.WriteLine(Average([3, 5, 9, -2]));
